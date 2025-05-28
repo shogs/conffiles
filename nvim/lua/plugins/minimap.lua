42c1b4b21e47d9fdab5106c7817640afc04137c7
Sa Sho <sash@shogs.me>
2025-05-28 11:46:19 +1000

    Minimap plugin added

---
+++ nvim/lua/plugins/minimap.lua
@@ -0,0 +1,17 @@
+return {
+  -- Minimap plugin
+  {
+    "gorbit99/codewindow.nvim",
+    config = function()
+      local codewindow = require("codewindow")
+      codewindow.setup({
+        auto_enable = true,
+        width_multiplier = 4, -- Width of the minimap
+        show_cursor = true,
+        screen_bounds = "background", -- Show the visible area
+        window_border = "single", -- Border style
+      })
+      codewindow.apply_default_keybinds()
+    end,
+  },
+}
