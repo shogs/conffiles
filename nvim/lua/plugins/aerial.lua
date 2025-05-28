9dc2c47be308d39023c949535d751a6e1f25e7b6
Sa Sho <sash@shogs.me>
2025-05-28 18:06:25 +1000

    Don't show variables, properties, fields, etc.

---
+++ nvim/lua/plugins/aerial.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "stevearc/aerial.nvim",
+    opts = {
+      filter_kind = {
+        "Class",
+        "Constructor",
+        "Enum",
+        "Function",
+        "Interface",
+        "Module",
+        "Method",
+        "Struct",
+      },
+    },
+  },
+}
