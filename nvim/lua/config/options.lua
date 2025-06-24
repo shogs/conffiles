90b8c72c43290e4c3529b2ee1f93c1f9bbf82f60
Sahand Shoghi <sahand.shoghi@yarris.com>
2025-06-25 07:38:32 +1000

    y stuff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 --
 
--- vim.o.tabstop = 4
+vim.o.tabstop = 4
 -- vim.o.expandtab = true
--- vim.o.softtabstop = 4
--- vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
