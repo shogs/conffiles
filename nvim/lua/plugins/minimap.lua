af8e018ac6cd584e66b8ae3da73fc309286df4d0
Sa Sho <sash@shogs.me>
2025-09-30 18:04:37 +1000

    Lazyvim snapshot

--- nvim/lua/plugins/minimap.lua
+++ nvim/lua/plugins/minimap.lua
@@ -1,17 +1,20 @@
-return {
-  -- Minimap plugin
-  {
-    "gorbit99/codewindow.nvim",
-    config = function()
-      local codewindow = require("codewindow")
-      codewindow.setup({
-        auto_enable = false,
-        width_multiplier = 4, -- Width of the minimap
-        show_cursor = true,
-        screen_bounds = "background", -- Show the visible area
-        window_border = "single", -- Border style
-      })
-      codewindow.apply_default_keybinds()
-    end,
-  },
-}
+if true then
+  return {}
+end
+-- return {
+--   -- Minimap plugin
+--   {
+--     "gorbit99/codewindow.nvim",
+--     config = function()
+--       local codewindow = require("codewindow")
+--       codewindow.setup({
+--         auto_enable = false,
+--         width_multiplier = 4, -- Width of the minimap
+--         show_cursor = true,
+--         screen_bounds = "background", -- Show the visible area
+--         window_border = "single", -- Border style
+--       })
+--       codewindow.apply_default_keybinds()
+--     end,
+--   },
+-- }
