76162d30cf5973938cdf4ca4ee4ecda1b5f85504
Sahand Shoghi <sahand.shoghi@yarris.com>
2025-10-06 09:29:34 +1100

    Zed and nvim lock snapshots

--- nvim/lua/plugins/minimap.lua
+++ nvim/lua/plugins/minimap.lua
@@ -1,20 +1,20 @@
 if true then
   return {}
 end
--- return {
---   -- Minimap plugin
---   {
---     "gorbit99/codewindow.nvim",
---     config = function()
---       local codewindow = require("codewindow")
---       codewindow.setup({
---         auto_enable = false,
---         width_multiplier = 4, -- Width of the minimap
---         show_cursor = true,
---         screen_bounds = "background", -- Show the visible area
---         window_border = "single", -- Border style
---       })
---       codewindow.apply_default_keybinds()
---     end,
---   },
--- }
+return {
+  -- Minimap plugin
+  {
+    "gorbit99/codewindow.nvim",
+    config = function()
+      local codewindow = require("codewindow")
+      codewindow.setup({
+        auto_enable = false,
+        width_multiplier = 4, -- Width of the minimap
+        show_cursor = true,
+        screen_bounds = "background", -- Show the visible area
+        window_border = "single", -- Border style
+      })
+      codewindow.apply_default_keybinds()
+    end,
+  },
+}
